511d19c305efd452d3577c55ccc21b1de286221f
ManuelHMR <manuelhmrjf@gmail.com>
2022-04-23 19:40:45 -0300

    data storage & tweet post

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ app.use(cors());
 app.use(express.json())
 app.listen(5000);
 
+let user = {};
 let users = [];
+let tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const {username, avatar} = req.body;
     users.push({username, avatar})
+    user = {username, avatar};
+    res.send("ok")
+});
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets)
+});
+
+app.post("/tweets", (req, res) => {
+    const {avatar} = user;
+    const {username, tweet} = req.body;
+    tweets.push({username, avatar,tweet})
     res.send("ok")
 });
